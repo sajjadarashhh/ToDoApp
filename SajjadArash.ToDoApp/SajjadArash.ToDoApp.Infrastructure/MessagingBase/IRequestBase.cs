354147afa117366c9514267e9544d959e0948ce2
سجاد Arash <72276897+sajjadarashhh@users.noreply.github.com>
2023-04-26 02:58:47 +0430

    IRequestBase: پیاده سازی تقریبا کامل شد بقیش در حین انجام پروژه انجام میشه اگه لازم شد داکیومنت ها نوشته شد.

--- SajjadArash.ToDoApp/SajjadArash.ToDoApp.Infrastructure/MessagingBase/IRequestBase.cs
+++ SajjadArash.ToDoApp/SajjadArash.ToDoApp.Infrastructure/MessagingBase/IRequestBase.cs
@@ -2,10 +2,50 @@
 
 namespace SajjadArash.ToDoApp.Infrastructure.MessagingBase
 {
-    public interface IRequestBase : IRequest
+    /// <summary>
+    /// ساختار درخواست های پروژه
+    /// </summary>
+    public interface IRequestBase : IRequest<IResponseBase>
     {
     }
-    public interface IRequestBase<TEntity> : IRequest<TEntity> where TEntity : IViewModelBase
+    /// <summary>
+    /// <inheritdoc cref="IRequestBase"/>
+    /// </summary>
+    /// <typeparam name="TEntity">نوع ورودی کاربر</typeparam>
+    public interface IRequestBase<TEntity> : IRequestBase where TEntity : IViewModelBase
+    {
+        public TEntity Entity { get; set; }
+    }
+    /// <summary>
+    /// <inheritdoc cref="IRequestBase"/>
+    /// </summary>
+    /// <typeparam name="TEntity">ورودی کاربر</typeparam>
+    /// <typeparam name="TResponse">خروجی</typeparam>
+    public interface IRequestBase<TEntity, TResponse> : IRequestBase<TEntity> where TEntity : IViewModelBase where TResponse : IViewModelBase
+    {
+
+    }
+    /// <summary>
+    /// <inheritdoc cref="IRequestBase"/>
+    /// </summary>
+    public interface IRequestPagingBase : IRequestBase
+    {
+        public int Size { get; set; }
+        public int Index { get; set; }
+    }
+    /// <summary>
+    /// <inheritdoc cref="IRequestBase"/>
+    /// </summary>
+    /// <typeparam name="TEntity"><inheritdoc cref="IRequestBase{TEntity}"/></typeparam>
+    public interface IRequestPagingBase<TEntity> : IRequestPagingBase, IRequestBase<TEntity> where TEntity : IViewModelBase
+    {
+    }
+    /// <summary>
+    /// <inheritdoc cref="IRequestBase"/>
+    /// </summary>
+    /// <typeparam name="TEntity">ورودی کاربر</typeparam>
+    /// <typeparam name="TResponse">خروجی کاربر</typeparam>
+    public interface IRequestPagingBase<TEntity, TResponse> : IRequestPagingBase, IRequestBase<TEntity, TResponse> where TEntity : IViewModelBase where TResponse : IViewModelBase
     {
     }
 }
