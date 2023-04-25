cefe1af0240327bc89d3b7634337abf20f9b35a8
سجاد Arash <72276897+sajjadarashhh@users.noreply.github.com>
2023-04-26 02:19:44 +0430

    First Commit : 1- Project Created Layers: 1- Application: represent application asp.net Core api 2- Services: represent application logics depent 3- Model: Implementation of Database that represent Tables and DB Type and conversions 4- infrastructure: represent common objects

---
+++ SajjadArash.ToDoApp/SajjadArash.ToDoApp.Infrastructure/RequestBase/IRequestBase.cs
@@ -0,0 +1,10 @@
+﻿namespace SajjadArash.ToDoApp.Infrastructure.RequestBase
+{
+    public interface IRequestBase
+    {
+    }
+    public interface IRequestBase<TEntity> : IRequestBase
+    {
+
+    }
+}
