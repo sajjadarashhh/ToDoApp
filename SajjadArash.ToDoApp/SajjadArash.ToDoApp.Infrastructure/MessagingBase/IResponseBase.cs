0fc4fe24b7097d3d53ca789a8804ba1e23ba2482
سجاد Arash <72276897+sajjadarashhh@users.noreply.github.com>
2023-04-26 02:39:24 +0430

    IResponseBase: یکپارچه سازی خروجی های پروژه

---
+++ SajjadArash.ToDoApp/SajjadArash.ToDoApp.Infrastructure/MessagingBase/IResponseBase.cs
@@ -0,0 +1,17 @@
+﻿using SajjadArash.ToDoApp.Infrastructure.Enums;
+
+namespace SajjadArash.ToDoApp.Infrastructure.MessagingBase
+{
+    public interface IResponseBase
+    {
+        string Message { get; set; }
+        bool IsSuccess { get; set; }
+        ApplicationCodes Code { get; set; }
+    }
+    public class ResponseBase : IResponseBase
+    {
+        public string Message { get; set; }
+        public bool IsSuccess { get; set; }
+        public ApplicationCodes Code { get; set; }
+    }
+}
