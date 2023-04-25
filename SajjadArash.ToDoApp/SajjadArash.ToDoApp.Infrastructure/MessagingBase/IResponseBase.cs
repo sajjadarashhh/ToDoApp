083664b06af096bd33f75b336d45798da9fd788b
سجاد Arash <72276897+sajjadarashhh@users.noreply.github.com>
2023-04-26 03:00:10 +0430

    IResponseBase: تقریبا کامل شده

--- SajjadArash.ToDoApp/SajjadArash.ToDoApp.Infrastructure/MessagingBase/IResponseBase.cs
+++ SajjadArash.ToDoApp/SajjadArash.ToDoApp.Infrastructure/MessagingBase/IResponseBase.cs
@@ -2,16 +2,26 @@
 
 namespace SajjadArash.ToDoApp.Infrastructure.MessagingBase
 {
+    /// <summary>
+    /// ساختار کلی خروجی های پروژه
+    /// </summary>
     public interface IResponseBase
     {
         string Message { get; set; }
         bool IsSuccess { get; set; }
         ApplicationCodes Code { get; set; }
     }
+    /// <summary>
+    /// <inheritdoc cref="IResponseBase"/>
+    /// </summary>
+    /// <typeparam name="TEntity">خروجی</typeparam>
     public interface IResponseBase<TEntity> : IResponseBase
     {
         public IEnumerable<TEntity> Entities { get; set; }
     }
+    /// <summary>
+    /// <inheritdoc cref="IResponseBase"/>
+    /// </summary>
     public interface IResponsePagingBase : IResponseBase
     {
         public int Index { get; set; }
@@ -19,6 +29,10 @@ namespace SajjadArash.ToDoApp.Infrastructure.MessagingBase
         public int TotalFiltered { get; set; }
         public int Total { get; set; }
     }
+    /// <summary>
+    /// <inheritdoc cref="IResponseBase"/>
+    /// </summary>
+    /// <typeparam name="TEntity"><inheritdoc cref="IResponseBase{TEntity}"/></typeparam>
     public interface IResponsePagingSize<TEntity> : IResponseBase<TEntity>, IResponsePagingBase
     {
 
