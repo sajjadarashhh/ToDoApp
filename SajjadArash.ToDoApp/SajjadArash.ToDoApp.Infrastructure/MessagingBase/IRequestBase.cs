f9eb947deb28606d624643b4bff25d95fadbb774
سجاد Arash <72276897+sajjadarashhh@users.noreply.github.com>
2023-04-26 02:34:15 +0430

    IViewModelBase: یک پارچه سازی درخواست های دریافتی

--- SajjadArash.ToDoApp/SajjadArash.ToDoApp.Infrastructure/MessagingBase/IRequestBase.cs
+++ SajjadArash.ToDoApp/SajjadArash.ToDoApp.Infrastructure/MessagingBase/IRequestBase.cs
@@ -1,6 +1,11 @@
-﻿namespace SajjadArash.ToDoApp.Infrastructure.MessagingBase
+﻿using MediatR;
+
+namespace SajjadArash.ToDoApp.Infrastructure.MessagingBase
 {
-    public interface IRequestBase
+    public interface IRequestBase : IRequest
+    {
+    }
+    public interface IRequestBase<TEntity> : IRequest<TEntity> where TEntity : IViewModelBase
     {
     }
 }
