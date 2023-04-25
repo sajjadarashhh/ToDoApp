08b39532039dd2b4f0fe5856c87c9bc4109431e8
سجاد Arash <72276897+sajjadarashhh@users.noreply.github.com>
2023-04-26 03:01:31 +0430

    IViewModelBase: این کلاس هم تقریبا کامل شده

--- SajjadArash.ToDoApp/SajjadArash.ToDoApp.Infrastructure/MessagingBase/IViewModelBase.cs
+++ SajjadArash.ToDoApp/SajjadArash.ToDoApp.Infrastructure/MessagingBase/IViewModelBase.cs
@@ -1,5 +1,8 @@
 ﻿namespace SajjadArash.ToDoApp.Infrastructure.MessagingBase
 {
+    /// <summary>
+    /// ساختار ورودی های کاربر و خروجی هایی که کاربر دریافت میکند
+    /// </summary>
     public interface IViewModelBase
     {
     }
